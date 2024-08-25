c19a124dcbce7a9c53afa3653f1018b848095d2a
Maxine Reyes Gomez <rg.maxine100@gmail.com>
2024-08-25 14:51:42 -0400

    Rename GLFW import to glfw from c

--- src/main.zig
+++ src/main.zig
@@ -1,50 +1,55 @@
 const std = @import("std");
 const gl = @import("zgl");
-const c = @cImport({
+const glfw = @cImport({
     @cInclude("GLFW/glfw3.h");
 });
 
 pub fn main() !void {
-    if (c.glfwInit() == 0) {
+    if (glfw.glfwInit() == 0) {
         std.debug.panic("Error: GLFW Init failed", .{});
     }
-    defer c.glfwTerminate();
+    defer glfw.glfwTerminate();
 
-    _ = c.glfwSetErrorCallback(errorCallback);
+    _ = glfw.glfwSetErrorCallback(errorCallback);
 
-    const window: ?*c.struct_GLFWwindow = c.glfwCreateWindow(800, 640, "ZigTracer", null, null);
+    const window: ?*glfw.struct_GLFWwindow = glfw.glfwCreateWindow(800, 640, "ZigTracer", null, null);
     if (window == undefined) {
         std.debug.panic("Error: Window or OpenGL context creation failed", .{});
     }
-    defer c.glfwDestroyWindow(window);
+    defer glfw.glfwDestroyWindow(window);
+
+    glfw.glfwMakeContextCurrent(window);
 
     // Enable VSync
-    c.glfwSwapInterval(1);
+    glfw.glfwSwapInterval(1);
 
     //----- end window setup -----
     //
-    // const vertices: [6]gl.Float = .{
-    //     0.0, 0.5, // Vertex 1 (X, Y)
-    //     0.5, -0.5, // Vertex 2 (X, Y)
-    //     -0.5, -0.5, // Vertex 3 (X, Y)
-    // };
-    // _ = vertices;
-    //
-    // // const vbo: []gl.Buffer = undefined;
-    // // gl.createBuffers(&vbo); // make Vertex Buffer Object (comptime int)
-    // // gl.bindBuffer(vbo[0], gl.BufferTarget.array_buffer); // make vbo active
-    // //
-    // // gl.bufferData(gl.BufferTarget.array_buffer, gl.Float, &vertices, gl.BufferUsage.static_draw);
+    const vertices: [6]gl.Float = .{
+        0.0, 0.5, // Vertex 1 (X, Y)
+        0.5, -0.5, // Vertex 2 (X, Y)
+        -0.5, -0.5, // Vertex 3 (X, Y)
+    };
+
+    var vbo: gl.UInt = undefined;
+    gl.genBuffers(vbo);
+
+    gl.createBuffers(&vbo); // make Vertex Buffer Object (comptime int)
+    gl.bindBuffer(vbo[0], gl.BufferTarget.array_buffer); // make vbo active
+
+    gl.bufferData(gl.BufferTarget.array_buffer, gl.Float, &vertices, gl.BufferUsage.static_draw);
     //
     // // gl.clearColor(0.2, 0.3, 0.3, 1.0);
     // // gl.clear(gl.binding.COLOR_BUFFER_BIT);
 
     // ----- begin main loop -----
 
-    while (c.glfwWindowShouldClose(window) == 0) {
-        if (c.glfwGetKey(window, c.GLFW_KEY_ESCAPE) == c.GLFW_PRESS) {
-            c.glfwSetWindowShouldClose(window, 1);
+    while (glfw.glfwWindowShouldClose(window) == 0) {
+        if (glfw.glfwGetKey(window, glfw.GLFW_KEY_ESCAPE) == glfw.GLFW_PRESS) {
+            glfw.glfwSetWindowShouldClose(window, 1);
         }
+        glfw.glfwSwapBuffers(window);
+        glfw.glfwPollEvents();
     }
 }
 
