5278574b68c7c18e9ffb5f8e93b6bca2dbecfc0c
Maxine Reyes Gomez <rg.maxine100@gmail.com>
2024-08-25 16:33:07 -0400

    Import GLEW and change all function calls and types accordingly

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,13 @@
 const std = @import("std");
-const gl = @import("zgl");
+const gl = @cImport({
+    @cInclude("GL/glew.h");
+});
 const glfw = @cImport({
     @cInclude("GLFW/glfw3.h");
 });
 
 pub fn main() !void {
+    // @setRuntimeSafety(false);
     if (glfw.glfwInit() == 0) {
         std.debug.panic("Error: GLFW Init failed", .{});
     }
@@ -23,30 +26,36 @@ pub fn main() !void {
     // Enable VSync
     glfw.glfwSwapInterval(1);
 
+    gl.glewExperimental = gl.GL_TRUE;
+    _ = gl.glewInit();
+
     //----- end window setup -----
-    //
-    const vertices: [6]gl.Float = .{
+
+    var width: c_int = undefined;
+    var height: c_int = undefined;
+    glfw.glfwGetFramebufferSize(window, &width, &height);
+    gl.glViewport(0, 0, width, height);
+
+    const vertices: [6]gl.GLfloat = .{
         0.0, 0.5, // Vertex 1 (X, Y)
         0.5, -0.5, // Vertex 2 (X, Y)
         -0.5, -0.5, // Vertex 3 (X, Y)
     };
 
-    var vbo: gl.UInt = undefined;
-    gl.genBuffers(vbo);
+    var vertex_buffer: gl.GLuint = undefined;
+    gl.glCreateBuffers().?(1, &vertex_buffer);
+    gl.glBindBuffer().?(gl.GL_ARRAY_BUFFER, vertex_buffer); // make vbo active
 
-    gl.createBuffers(&vbo); // make Vertex Buffer Object (comptime int)
-    gl.bindBuffer(vbo[0], gl.BufferTarget.array_buffer); // make vbo active
+    gl.glBufferData().?(gl.GL_ARRAY_BUFFER, @sizeOf(gl.GLfloat), &vertices, gl.GL_STATIC_DRAW);
 
-    gl.bufferData(gl.BufferTarget.array_buffer, gl.Float, &vertices, gl.BufferUsage.static_draw);
-    //
-    // // gl.clearColor(0.2, 0.3, 0.3, 1.0);
-    // // gl.clear(gl.binding.COLOR_BUFFER_BIT);
+    gl.glClearColor(0.2, 0.3, 0.3, 1.0);
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT);
 
     // ----- begin main loop -----
 
     while (glfw.glfwWindowShouldClose(window) == 0) {
-        if (glfw.glfwGetKey(window, glfw.GLFW_KEY_ESCAPE) == glfw.GLFW_PRESS) {
-            glfw.glfwSetWindowShouldClose(window, 1);
+        if (glfw.glfwGetKey(window, glfw.GLFW_KEY_CAPS_LOCK) == glfw.GLFW_PRESS) {
+            glfw.glfwSetWindowShouldClose(window, glfw.GL_TRUE);
         }
         glfw.glfwSwapBuffers(window);
         glfw.glfwPollEvents();
@@ -59,6 +68,11 @@ test "simple test" {
     try list.append(42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
+
+const ConvertError = error{
+    TooBig,
+    TooSmall,
+};
 
 // Default GLFW error handling callback
 fn errorCallback(error_code: c_int, description: [*c]const u8) callconv(.C) void {
