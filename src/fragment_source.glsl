35ddc8c7573d972ae38915dd52018bbd29106d32
Maxine Reyes Gomez <rg.maxine100@gmail.com>
2024-08-28 15:02:00 -0400

    Reformat shaders

--- src/fragment_source.glsl
+++ src/fragment_source.glsl
@@ -5,6 +5,5 @@ in vec3 Color;
 out vec4 outColor;
 
 void main() {
-
     outColor = vec4(Color, 1.0);
 }
--- src/vertex_source.glsl
+++ src/vertex_source.glsl
@@ -6,9 +6,6 @@ in vec3 color;
 out vec3 Color;
 
 void main() {
-    // Color.r = color.r * abs(position.y);
-    // Color.g = color.g * abs(position.y);
-    // Color.b = color.b * abs(position.y);
     Color = color;
     gl_Position = vec4(position, 1.0);
 }
